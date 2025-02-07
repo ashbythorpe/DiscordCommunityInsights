141ce6ba7af2f4b070a81ebd31e9c0484e6ab38d
Ashby Thorpe <ashbythorpe@gmail.com>
2025-02-07 21:58:19 +0000

    add basic Discord bot

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Don't kill yourself!")
+
+
+token = os.getenv("DISCORD_TOKEN")
+
+if token is None:
+    raise ValueError("DISCORD_TOKEN is not set")
+
+client.run(token)
