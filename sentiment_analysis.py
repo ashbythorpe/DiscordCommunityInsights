a9a5eab0ad590645e0234af0d0938f42989ad2fd
Parthiv <94118901+deyparthiv@users.noreply.github.com>
2025-02-08 08:52:21 +0000

    new read from database function

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,27 @@
+import sqlite3
 from transformers import pipeline
+
+DATABASE_FILE = "database.db"
+
+def read_messages():
+    """Connect to the database and read all messages"""
+    try:
+        conn = sqlite3.connect(DATABASE_FILE)
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM Messages")
+        messages = cursor.fetchall()
+
+        print("Messages:")
+        for msg in messages:
+            print(msg)
+
+        conn.close()
+    except sqlite3.Error as e:
+        print(e)
 
 sentiment_pipeline = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
 data = ["I love you", "I hate you", "I don't know"]
 print(sentiment_pipeline(data))
+
 
